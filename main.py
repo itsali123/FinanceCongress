a5268a48d480223c7291e2bd91543c871309599b
Ali Hussain <ahh1502@gmail.com>
2022-05-17 16:46:13 +0100

    Implement basic stock scraper

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
-print("hello world")
+import pandas as pd
+import pandas_datareader.data as web
+from pandas import Series, DataFrame
+import datetime
+from datetime import date
+
+# Get stock from user
+stock = input("Please enter your stock: ")
+
+# Specify date range
+start = datetime.datetime(2020, 2, 2)
+end = datetime.datetime(2021, 2, 2)
+
+# Grab stock info from yahoo, start to end
+df = web.DataReader(stock, 'yahoo', start, end)
+print(df)
